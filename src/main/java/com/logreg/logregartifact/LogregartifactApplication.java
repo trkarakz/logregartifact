53fa1f2f73416cf93ff443f2f5b2836d63bf53dd
Zülfikar Karakaya <mac@Zuzu-Mac.local>
2023-04-27 20:27:45 +0200

    inital commit

---
+++ src/main/java/com/logreg/logregartifact/LogregartifactApplication.java
@@ -0,0 +1,13 @@
+package com.logreg.logregartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogregartifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogregartifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logreg/logregartifact/LogregartifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logreg.logregartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogregartifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
